2a4f08355924532168ef14c39359edeec337cc97
Dhasarathan <112262869+techdhasarthan@users.noreply.github.com>
2025-06-16 22:38:32 +0530

    Updated _layout.jsx

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, MapPin, User } from 'lucide-react-native';
+import { Chrome as Home, MapPin, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
